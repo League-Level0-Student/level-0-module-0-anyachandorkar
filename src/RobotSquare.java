5870da2528fa325818ad331e04ba9ec256fb21c1
cindyfeldman <league@wts21.attlocal.net>
2017-12-02 12:59:08 -0800

    Module 0

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -3,6 +3,8 @@
  *    Level 0
  */
 
+import java.awt.font.FontRenderContext;
+
 import org.jointheleague.graphical.robot.Robot;
 
 // This recipe draws a square using the Robot
@@ -11,22 +13,24 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot walle = new Robot();
 
         // 3. Put the robot's pen down
-
+walle.penDown();
 
         // 6. Make the robot move as fast as possible
-
+walle.setSpeed(100);
 
         // 5. Do everything below here 4 times
+for (int i = 0; i < 4; i++) {
+	
 
 
         //         2. Move your robot 200 pixels
-
+walle.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
+walle.turn(90);
 
-
-    }
+}}
 }
